14c6fad5b73fbe0086cd9d5c68c753daade84bfd
Kubsonekk <68493783+Kubsonekkmusic@users.noreply.github.com>
2020-11-16 10:12:33 +0100

    funkcje

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export const addTwoNumbers = (a: number, b: number): number => {
+    return a + b;
+  }
+  
+  export const logMessage = (message: string): void => {
+    console.log(message.toUpperCase());
+  }
+  
